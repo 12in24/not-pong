7e95c98b30c20232f9d445054296df6d43e872f9
Anthony Kesich <akesich+github@gmail.com>
2024-01-20 21:03:15 -0600

    Add second paddle and control it with w/s

--- scenes/paddle_sprite.gd
+++ scenes/paddle_sprite.gd
@@ -1,13 +1,28 @@
 extends CharacterBody2D
 
 
+@export_range(1, 2) var player: int = 1
+
+var input_up: String
+var input_down: String
+
+
 const SPEED = 300.0
+
+
+func _ready():
+	if player == 1:
+		input_up = "player1_up"
+		input_down = "player1_down"
+	else:
+		input_up = "player2_up"
+		input_down = "player2_down"
 
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("up", "down")
+	var direction = Input.get_axis(input_up, input_down)
 	if direction:
 		velocity.y = direction * SPEED
 	else:
