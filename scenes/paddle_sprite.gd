f2fe8eb1af6fcded1065ab320bb1a3501d201ae7
Anthony Kesich <akesich+github@gmail.com>
2024-01-25 20:36:18 -0600

    Let paddles rotate

--- scenes/paddle_sprite.gd
+++ scenes/paddle_sprite.gd
@@ -5,18 +5,25 @@ extends CharacterBody2D
 
 var input_up: String
 var input_down: String
+var input_clockwise: String
+var input_counterclockwise: String
 
 
-const SPEED = 300.0
+const SPEED = 400.0
+const OMEGA = 5.0
 
 
 func _ready():
 	if player == 1:
 		input_up = "player1_up"
 		input_down = "player1_down"
+		input_clockwise = "player1_clockwise"
+		input_counterclockwise = "player1_counterclockwise"
 	else:
 		input_up = "player2_up"
 		input_down = "player2_down"
+		input_clockwise = "player2_clockwise"
+		input_counterclockwise = "player2_counterclockwise"
 
 
 func _physics_process(delta):
@@ -27,6 +34,11 @@ func _physics_process(delta):
 		velocity.y = direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
+		
+	var rotation_dir = Input.get_axis(input_counterclockwise, input_clockwise)
+	
+	if rotation:
+		self.rotation += OMEGA * rotation_dir * delta
 
 	move_and_collide(delta * velocity)
 
